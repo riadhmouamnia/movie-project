053a7931dba7d249c7cd4788bb9e34bd141b0b21
mohamed <matassi.mohamed89@gmail.com>
2023-09-15 16:53:07 +0100

    feat(footer): create footer component
    fix #5

--- src/util/constants.js
+++ src/util/constants.js
@@ -5,3 +5,59 @@ export const moviesList = [
   { id: 4, name: "Now playing" },
   { id: 5, name: "Upcoming" },
 ]
+
+export const teamMembers = [
+  {
+    id: 1,
+    name: "Riadh Mouamnia",
+    github: "https://github.com/riadhmouamnia",
+    linkedin: " https://www.linkedin.com/in/riadh-mouamnia",
+    imgSrc:
+      "https://logowik.com/content/uploads/images/github-circle-icon1425.jpg",
+  },
+  {
+    id: 2,
+    name: "Mohamed Matassi",
+    github: "https://github.com/M07am3dM",
+    linkedin: "https://www.linkedin.com/in/matassi-mohamed/",
+    imgSrc:
+      "https://media.licdn.com/dms/image/D4E03AQF5osmJLYc_WQ/profile-displayphoto-shrink_100_100/0/1680787767230?e=1700092800&v=beta&t=iV6EiNamc9HCLL0f4RM8V3JNBT0vSpXb--I2WTklr0Q",
+  },
+  {
+    id: 3,
+    name: "Meriem Mansouri",
+    github: "https://github.com/meryem08",
+    linkedin: "https://www.linkedin.com/in/meriem-mansouri-1b1643276/",
+    imgSrc:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRZEWt9Dr4D_DIzdIrHERkTyeduD4eeuDF2lfX9ojX4UW7WytlfAEB5BALZcKCG-Ahglvo&usqp=CAU",
+  },
+  {
+    id: 4,
+    name: "Mohamed Matassi",
+    github: "https://github.com/M07am3dM",
+    linkedin: "https://www.linkedin.com/in/matassi-mohamed/",
+    imgSrc:
+      "https://media.licdn.com/dms/image/D4E03AQF5osmJLYc_WQ/profile-displayphoto-shrink_100_100/0/1680787767230?e=1700092800&v=beta&t=iV6EiNamc9HCLL0f4RM8V3JNBT0vSpXb--I2WTklr0Q",
+  },
+  {
+    id: 5,
+    name: "Meriem Mansouri",
+    github: "https://github.com/meryem08",
+    linkedin: "https://www.linkedin.com/in/meriem-mansouri-1b1643276/",
+    imgSrc:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRZEWt9Dr4D_DIzdIrHERkTyeduD4eeuDF2lfX9ojX4UW7WytlfAEB5BALZcKCG-Ahglvo&usqp=CAU",
+  },
+]
+
+export const about = [
+  {
+    id: 1,
+    name: "GitHub",
+    link: "https://github.com/202306-NEA-DZ-FEW/movie-project-popcornpalace",
+  },
+  {
+    id: 2,
+    name: "Re-Coded",
+    link: "https://www.re-coded.com/?gclid=CjwKCAjwgZCoBhBnEiwAz35RwhQwjUzfPnFeW_ws4Aj-az23eS7m2QoLnP5XgOH_3JNUESjCyyJvhBoCEFwQAvD_BwE",
+  },
+]
