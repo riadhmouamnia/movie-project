8a508ef0cec9967a22154abfa592e39c28b9fcb9
Riadh Mouamnia <riadh.mouamnia@gmail.com>
2023-09-19 00:37:32 +0100

    feat(project): add head component and favicon.ico
    add head to pages for better SEO optimization and add favicon.ico file
    
    fix #41

--- components/ActorCard/ActorCardPlaceHolder.jsx
+++
@@ -1,9 +0,0 @@
-const ActorCardPlaceHolder = () => {
-  return (
-    <div className="w-[160px] sm:w-[200px] md:w-[240px] lg:w-[280px] inline-block cursor-pointer relative p-2">
-      <div className="w-full h-[400px] bg-gray-700 animate-pulse" />
-    </div>
-  )
-}
-
-export default ActorCardPlaceHolder
--- src/pages/movies/[movieId].jsx
+++ src/pages/movies/[movieId].jsx
@@ -10,6 +10,7 @@ import {
   getMovieTrailer,
   getSimilarMovies,
 } from "@/util/API"
+import Head from "next/head"
 
 function MovieDetails({ movie, similarMovies, trailer, actors }) {
   const [releseYear] = movie.release_date.split("-")
@@ -18,39 +19,46 @@ function MovieDetails({ movie, similarMovies, trailer, actors }) {
   const formattedRuntime = `${hours}h ${minutes}min`
 
   return (
-    <main className="p-4 grid xl:grid-cols-6 xl:p-10 xl:my-10 grid-cols-1 sm:grid-col-1 gap-6 md:grid-cols-2 md:mt-16">
-      <div className="w-full h-[620px] md:min-h-[600px] xl:min-h-[660px] xl:col-span-2 relative">
-        <div className="absolute w-full h-[20%] bg-gradient-to-b from-black opacity-60 to-transparent z-10"></div>
-        <Image
-          fill
-          sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-          className=" object-cover"
-          src={`${IMAGE_BASE_URL_ORIGINAL}${movie?.poster_path}`}
-          alt="cover image"
-          priority
-        />
-        <div className="absolute bottom-0 w-full h-[20%] bg-gradient-to-t from-black opacity-60 to-transparent z-10"></div>
-      </div>
-      <div className="xl:col-span-4">
-        <h1 className="dark:text-white text-4xl mb-4 font-bold flex justify-between">
-          {movie.original_title}
-          <span className="flex items-center gap-2">
-            {movie.vote_average} <BsFillStarFill className="text-yellow-500" />
-          </span>
-        </h1>
-        <p className="text-gray-400 text-sm flex items-center gap-2 mb-10">
-          <span>{releseYear}</span>|<span>{formattedRuntime}</span>|
-          <span>{movie.adult ? "18+" : "16+"}</span>
-        </p>
-        {/* Tabs */}
-        <Tabs
-          movie={movie}
-          similarMovies={similarMovies}
-          trailer={trailer}
-          actors={actors}
-        />
-      </div>
-    </main>
+    <>
+      <Head>
+        <title>{movie.original_title}</title>
+        <meta name="description" content={movie.overview} />
+      </Head>
+      <main className="p-4 grid xl:grid-cols-6 xl:p-10 xl:my-10 grid-cols-1 sm:grid-col-1 gap-6 md:grid-cols-2 md:mt-16">
+        <div className="w-full h-[620px] md:min-h-[600px] xl:min-h-[660px] xl:col-span-2 relative">
+          <div className="absolute w-full h-[20%] bg-gradient-to-b from-black opacity-60 to-transparent z-10"></div>
+          <Image
+            fill
+            sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+            className=" object-cover"
+            src={`${IMAGE_BASE_URL_ORIGINAL}${movie?.poster_path}`}
+            alt="cover image"
+            priority
+          />
+          <div className="absolute bottom-0 w-full h-[20%] bg-gradient-to-t from-black opacity-60 to-transparent z-10"></div>
+        </div>
+        <div className="xl:col-span-4">
+          <h1 className="dark:text-white text-4xl mb-4 font-bold flex justify-between">
+            {movie.original_title}
+            <span className="flex items-center gap-2">
+              {movie.vote_average}{" "}
+              <BsFillStarFill className="text-yellow-500" />
+            </span>
+          </h1>
+          <p className="text-gray-400 text-sm flex items-center gap-2 mb-10">
+            <span>{releseYear}</span>|<span>{formattedRuntime}</span>|
+            <span>{movie.adult ? "18+" : "16+"}</span>
+          </p>
+          {/* Tabs */}
+          <Tabs
+            movie={movie}
+            similarMovies={similarMovies}
+            trailer={trailer}
+            actors={actors}
+          />
+        </div>
+      </main>
+    </>
   )
 }
 
